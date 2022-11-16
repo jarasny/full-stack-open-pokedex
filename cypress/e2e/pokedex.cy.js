a71323ee27f9fb6a7822a3bb89986a6d7cc6ce65
jarasny <jarazny.mateusz@outlook.com>
2022-11-16 19:10:19 +0100

    Added one more test, fixed action version

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from main page to pokemon page', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item-name').contains('rattata').click()
+    cy.location('pathname', { timeout: 10000 }).should('include', '/pokemon/rattata')
+    cy.contains('hustle')
+    cy.contains('run away')
+  })
 })
